53f0980818761383bdf5dbfe3e298663c2b71f57
Guillaume <gthvn1@gmail.com>
2023-01-23 14:48:25 +0100

    from_grub: minor updates

--- from_grub/multiboot_header.asm
+++ from_grub/multiboot_header.asm
@@ -1,17 +1,19 @@
 ;; add an annotation to the section
 section .multiboot_header
 
-header_start:
-	dd 0xe85250d6  	;; the magic for multiboot header
-	dd 0		;; we want to boot in protected mode (tell it to grub)
-	dd header_end - header_start ;; length of the header
-
+;; See https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html
+header:
+	.start:
+	dd 0xE85250D6	;; the magic for multiboot2 header
+	dd 0x0		;; the field arch: 0 => 3 bit protected mode fir i386
+	dd header.end - header.start ;; length of the header including magic fields
 	;; grub also requires a fourth parameter that is the checksum
-	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+	;; with checksum = - (magic + arch + length)
+	dd 0x100000000 - (0xE85250D6 + 0x0 + (header.end - header.start))
 
 	;; required end tags
 	dw 0	;; type
 	dw 0	;; flags
 	dd 8	;; size
-header_end:
+	.end:
 
