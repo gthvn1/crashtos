0770953d41dacf36bf17026f3bf64ba8ab164109
Guillaume <gthvn1@gmail.com>
2023-02-03 18:10:50 +0100

    zigos: allow the build of zigos.iso

--- zigos/Readme.md
+++ zigos/Readme.md
@@ -1,4 +1,6 @@
-# OS Dev
+# ZigOS
+
+## Bare Bones tutorial
 
 Try the tutorial [Bare Bones](https://wiki.osdev.org/Bare_Bones) from OS dev.
 We try to use [zig](https://ziglang.org/) because there are some facilites to
@@ -7,3 +9,7 @@ build low level stuff and use some cool keyword like `linksection`...
 The goal is to
  - [ ] boot
  - [ ] print message
+
+ ## Build
+
+ `zig build`
--- zigos/build.zig
+++ zigos/build.zig
@@ -5,26 +5,44 @@ const CrossTarget = @import("std").zig.CrossTarget;
 const Feature = @import("std").Target.Cpu.Feature;
 
 pub fn build(b: *Builder) void {
-    const features = Target.x86.Feature;
-
-    var disabled_features = Feature.Set.empty;
-    var enabled_features = Feature.Set.empty;
-
-    disabled_features.addFeature(@enumToInt(features.mmx));
-    disabled_features.addFeature(@enumToInt(features.sse));
-    disabled_features.addFeature(@enumToInt(features.sse2));
-    disabled_features.addFeature(@enumToInt(features.avx));
-    disabled_features.addFeature(@enumToInt(features.avx2));
-    enabled_features.addFeature(@enumToInt(features.soft_float));
-
-    const target = CrossTarget{ .cpu_arch = Target.Cpu.Arch.i386, .os_tag = Target.Os.Tag.freestanding, .abi = Target.Abi.none, .cpu_features_sub = disabled_features, .cpu_features_add = enabled_features };
-
-    const mode = b.standardReleaseOptions();
 
     const kernel = b.addExecutable("kernel.elf", "src/boot.zig");
+
+    const target = CrossTarget{
+        .cpu_arch = Target.Cpu.Arch.i386,
+        .os_tag = Target.Os.Tag.freestanding,
+        .abi = Target.Abi.none
+    };
     kernel.setTarget(target);
+
+    const mode = b.standardReleaseOptions();
     kernel.setBuildMode(mode);
+
     kernel.setLinkerScriptPath(.{ .path = "src/linker.ld" });
     kernel.code_model = .kernel;
     kernel.install();
+
+    const kernel_step = b.step("kernel", "Build the kernel");
+    kernel_step.dependOn(&kernel.install_step.?.step);
+
+    const iso_dir = b.fmt("{s}/iso", .{b.cache_root});
+    const kernel_path = b.getInstallPath(kernel.install_step.?.dest_dir, kernel.out_filename);
+    const iso_path = b.fmt("{s}/zigos.iso", .{b.exe_dir});
+
+    const iso_cmd_str = &[_][]const u8{
+        "/bin/sh", "-c",
+        std.mem.concat(b.allocator, u8, &[_][]const u8{
+            "mkdir -p ", iso_dir, "/boot/grub && ",
+            "cp ", kernel_path, " ", iso_dir, "/boot && ",
+            "cp src/grub.cfg ", iso_dir, "/boot/grub && ",
+            "grub2-mkrescue -o ", iso_path, " ", iso_dir
+        }) catch unreachable
+    };
+
+    const iso_cmd = b.addSystemCommand(iso_cmd_str);
+    iso_cmd.step.dependOn(kernel_step);
+
+    const iso_step = b.step("iso", "Build an ISO image");
+    iso_step.dependOn(&iso_cmd.step);
+    b.default_step.dependOn(iso_step);
 }
