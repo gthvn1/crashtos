c9b7ffd6e74ab192e64a2dd51f396b36ffb5d4bc
Guillaume <gthvn1@gmail.com>
2023-02-17 14:09:01 +0100

    improve the hello a little bit

--- yak/kernel.zig
+++ yak/kernel.zig
@@ -2,7 +2,10 @@ const tty = @import("tty.zig");
 
 export fn kmain() callconv(.Naked) noreturn {
     tty.initialize();
-    tty.write("Welcome from zig", tty.VGAColor.Green, tty.VGAColor.Black);
+    tty.write("-= YaK version 0.1 =-", tty.VGAColor.LightBrown, tty.VGAColor.Blue);
+    tty.nextLine();
+    tty.nextLine();
+    tty.write(" Welcome from zig", tty.VGAColor.Green, tty.VGAColor.Black);
 
     while (true) {
         asm volatile ("hlt");
