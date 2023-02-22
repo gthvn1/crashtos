864e74166628c020f1099a269f34476123a34771
Guillaume <gthvn1@gmail.com>
2023-02-22 05:45:12 +0100

    crash_test_dummy: print from memory location

--- crash_test_dummy/boot.asm
+++ crash_test_dummy/boot.asm
@@ -13,6 +13,11 @@
 ;   BL = Foreground color (graphics mode only)
 ;   BH = Display page number (text modes only)
 
+	org 0x7C00 ; The code is loaded at 0x7C00 by the bootloader
+		   ; We need to set it otherwise when later in the code
+		   ; we will refer to memory location the address will be
+		   ; wrong. For example mov al, [outputChar] will not work.
+
 	mov ah, 0x0
 	mov al, 0x12
 	int 0x10 ; change mode to VGA 640x480 16 colors
@@ -20,14 +25,13 @@
 	mov bl, 0xa  ; Light Green
 
 	mov ah, 0x0e
-	mov al, 0x41 ; 'A'
-	int 0x10
-
-	mov al, 0x42 ; 'B'
+	mov al, [outputChar]
 	int 0x10
 
 loop:
 	jmp loop
+
+outputChar: db 'Z', 0
 
 	times 510-($-$$) db 0 ; padding with 0s
 	dw 0xaa55  ; BIOS magic number
