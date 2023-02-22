0b3531a6cdc0a78cd5c908443bd49576609551bc
Guillaume <gthvn1@gmail.com>
2023-02-22 06:19:56 +0100

    crash_test_dummy: print two strings

--- crash_test_dummy/boot.asm
+++ crash_test_dummy/boot.asm
@@ -18,28 +18,40 @@
 		   ; we will refer to memory location the address will be
 		   ; wrong. For example mov al, [outputChar] will not work.
 
+	; Set up mode to VGA 640x480 16 colors
 	mov ah, 0x0
 	mov al, 0x12
-	int 0x10 ; change mode to VGA 640x480 16 colors
-
-	mov ah, 0x0e ; Write Char on TTY service
-	mov bl, 0xa  ; Light Green
-	mov si, outputChars ; bx "points" to outputChars
-
-print_chars:
-	mov al, [si]
-	
-	or al, al ; Set ZF if it is zero
-	jz .end
 	int 0x10
-	inc si
-	jmp print_chars
-.end
+
+	; start writing something
+	mov ah, 0x0e ; code of the service to write Char
+	mov bl, 0xa  ; Light Green
+	mov si, helloStr ; bx "points" to helloStr
+	call print_string
+
+	mov si, worldStr
+	call print_string
 
 infinite_loop:
 	jmp infinite_loop
 
-outputChars: db 'A', 'B', 'C', 'D', 0
+; print_chars
+;  si: points to the beginning of the string and ends with '0'
+print_string:
+	mov al, [si]
+	or al, al ; Set ZF if it is zero
+	jnz print_char
+	ret
+print_char:
+	int 0x10
+	inc si
+	jmp print_string
+
+helloStr: db "Hello,", 0xa, 0 ; 0xa is line feed (move cursor down to next line)
+worldStr: db "World!", 0xa, 0xd, 0 ; 0xd is carriage return (return to the beginning)
+; We should see when printing:
+; Hello,
+;       World!
 
 	times 510-($-$$) db 0 ; padding with 0s
 	dw 0xaa55  ; BIOS magic number
