8e63da465690e7bc25486749950d69e82bcd36ed
Guillaume <gthvn1@gmail.com>
2023-02-24 17:04:19 +0100

    ctd-os: put the print_str in its file

--- crash_test_dummy/src/asm/kernel.asm
+++ crash_test_dummy/src/asm/kernel.asm
@@ -67,21 +67,8 @@ process_input:
 .reboot:
     jmp 0xFFFF:0x0000 ; jump to the vector reset
 
-; print_str
-; Inputs:
-;   - SI: contains address of the string to be printed
-; Clobbers:
-;   - SI, AX
-print_str:
-	mov ah, 0x0e ; Set BIOS Service to "write text in Teletype Mode"
-.get_next_char:
-	lodsb        ; al <- DS:SI and increment SI by one
-	or al, al    ; It will set ZF if it is zero
-	jz .done     ; And if it al == 0 we reach the end of the string
-	int 0x10     ; otherwise print the character and go to next one...
-	jmp .get_next_char
-.done:
-	ret
+;; As it is compile at the top we need to include the asm file with its path
+%include "src/asm/print_str.asm"
 
 menuString:
 	db "------------------------", 0xa, 0xd
---
+++ crash_test_dummy/src/asm/print_str.asm
@@ -0,0 +1,15 @@
+; print_str
+; Inputs:
+;   - SI: contains address of the string to be printed
+; Clobbers:
+;   - SI, AX
+print_str:
+	mov ah, 0x0e ; Set BIOS Service to "write text in Teletype Mode"
+.get_next_char:
+	lodsb        ; al <- DS:SI and increment SI by one
+	or al, al    ; It will set ZF if it is zero
+	jz .done     ; And if it al == 0 we reach the end of the string
+	int 0x10     ; otherwise print the character and go to next one...
+	jmp .get_next_char
+.done:
+	ret
