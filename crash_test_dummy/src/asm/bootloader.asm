7725b4a5a67bd0539916721d4fd3ec6d767707be
Guillaume <gthvn1@gmail.com>
2023-02-24 14:41:19 +0100

    ctd-os: cosmetic changes in bootloader

--- crash_test_dummy/src/asm/bootloader.asm
+++ crash_test_dummy/src/asm/bootloader.asm
@@ -3,20 +3,20 @@
 ;;
 ;; It will load the kernel by reading the second sector on the first
 ;; disk.
-;;
+
 	org 0x7C00 ; The code is loaded at 0x7C00 by the bootloader
-		   ; We need to set it otherwise when later in the code
-		   ; we will refer to memory location the address will be
-		   ; wrong. For example mov al, [outputChar] will not work.
+		       ; We need to set it otherwise when later in the code
+		       ; we will refer to memory location the address will be
+		       ; wrong. For example mov al, [outputChar] will not work.
 
 	xor bx, bx
-	mov es, bx      ; es <- 0x0000
-	mov bx, 0x7E00  ; Set [es:bx] to 0x7E00, this is where the data
-			; will be put when read from the disk. So it is
-			; where the kernel will be loaded.
+	mov es, bx     ; es <- 0x0000
+	mov bx, 0x7E00 ; Set [es:bx] to 0x7E00, this is where the data
+			       ; will be put when read from the disk. So it is
+			       ; where the kernel will be loaded.
 
-	call load_kernel_from_disk   ; Read the kernel from disk
-	jmp 0x7E00	; If it returns then jump to the kernel
+	call load_kernel_from_disk ; Read the kernel from disk
+	jmp 0x7E00	               ; once loaded jump to the kernel
 
 	; Should not be reached !!!
 	cli
@@ -26,7 +26,7 @@ load_kernel_from_disk:
 	;  - es:bx are set set before calling it
 	push si
 	mov si, 0x5 ; disk reads should be retried at least three times
-		    ; we use SI because all others registers are needed
+		        ; we use SI because all others registers are needed
 
 	; Reset the disk before reading it
 	mov ah, 0x0
@@ -36,7 +36,7 @@ load_kernel_from_disk:
 	mov al, 0x1  ; Only read 1 sector
 	mov ch, 0x0  ; Cylinder 0
 	mov cl, 0x3  ; Sector 3 (1 is bootloader and 2 is filetable)
-		     ; Remember, sector starts from 1
+		         ; Remember, sector starts from 1
 	mov dh, 0x0  ; Head 0
 	mov dl, 0x80 ; First hard drive
 
