ca3d029b94656ad35c4f204b6e2ffebadeea3a66
Guillaume Thouvenin <gthouvenin@kalray.eu>
2023-02-28 13:45:49 +0100

    ctd-os: Build and copy calculator to floppy

---
+++ crash_test_dummy/src/asm/calculator.asm
@@ -0,0 +1,6 @@
+;; ============================================================================
+;; calculator.asm
+db "TODO: calculator"
+
+	; Sector padding to have a bin generated of 512 bytes (one sector)
+	times 512-($-$$) db 0
