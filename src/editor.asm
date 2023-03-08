492872558f69dd580cdeb9f0379d5b4a9b23496d
Guillaume <gthvn1@gmail.com>
2023-03-08 13:00:02 +0100

    Start replacing int 16h
    Now we are using PIO to get the input from keyboard. This modification
    breaks the editor. We can load the editor but currently for debugging
    purpose we loop to read characters and never break the loop.

--- src/editor.asm
+++ src/editor.asm
@@ -8,9 +8,7 @@
 ;; Currently we only display a message and wait for a user input. Once user
 ;; hit a key we return to stage2. Nothing fancy...
 
-%include "include/constants.asm"
-
-org PROGRAM_OFFSET
+[ORG 0x0]
 
 ;; ----------------------------------------------------------------------------
 ;; MAIN
@@ -20,25 +18,34 @@ editor:
     ; In the editor we don't use BIOS interrupts for printing
     ; message. We use the Video Memory. We are still in 80x25
 
-    mov ax, VIDEO_MEMORY
+    mov ax, 0xB800
     mov es, ax  ; In real mode segment is shifted so es:0000 => 0xB8000
 
     ; To clean screen we can write 80x25 () spaces on the screen
+    ; We use our own clear screen to have different colors.
     mov al, ' '
-    mov ah, 0x1E
+    mov ah, 0x1E  ; BG: dark, FG: Yellow
     mov di, 0
-    mov cx, 2000   ; 80x25 (! it is not in hexa :)
-    rep stosw      ; store AX at ES:DI repeated 2000 times
+    mov cx, 80*25 ; 80x25 (! it is not in hexa :)
+    rep stosw     ; store AX at ES:DI repeated 2000 times
 
     mov si, editorHdr   ; display a welcome message
     call print_string
 
-    mov ah, 0x0  ; wait for keypress before jumping back to stage2
-    int 0x16     ; BIOS interrupt for keyboard services
+read_data:
+    ; Keyboard data port 0x60
+    in al, 0x60  ; Get key pressed scancode from data port
+
+    cmp al, [keyPressed]
+    je read_data
+
+    mov byte [keyPressed], al
+    call print_char
+    jmp read_data
 
 jmp_stage2:
     ; before jumping to the stage2 we need to setup segments
-    mov ax, STAGE2_SEG
+    mov ax, 0x1000
     mov ds, ax
     mov es, ax
     mov fs, ax
@@ -46,7 +53,7 @@ jmp_stage2:
 
     popa
 
-    jmp STAGE2_SEG:STAGE2_OFFSET ; far jump to stage2
+    jmp 0x1000:0x0200 ; far jump to stage2
 
 ;; ----------------------------------------------------------------------------
 ;; FUNCTIONS
@@ -67,6 +74,10 @@ print_string:
     ret
 
 print_char:
+    ; AL contains the scancode, do the translation
+    mov bx, scancodeTable
+    xlatb ; Use the content of AL to lookup in scancodeTable and write back the
+          ; contents
     mov ah, 0x1E; 0x1 is for blue background and 0xE is for yellow foreground
     mov cx, ax  ; save attribute (rememer ASCII has been loaded in AL) because
                 ; mul is using AX for the multiplication
@@ -95,9 +106,18 @@ print_char:
 ;; ----------------------------------------------------------------------------
 ;; VARIABLES
 
-editorHdr db "Inside ctd-editor !!!", 0
-xpos      db 0
-ypos      db 0
+editorHdr  db "Inside ctd-editor !!!", 0
+xpos       db 0
+ypos       db 0
+keyPressed db 0
 
-    ; Sector padding to have a bin generated of 512 bytes (one sector)
+; Scancode table is used with xlatb to locates a byte entry using the content
+; of AL. We setup the table using our azerty layout...
+scancodeTable:
+    db 00h, 01h, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0Eh, 0Fh
+    db 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', 1Ch, 1Dh, 'q', 's'
+    db 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'ù', '*', 2Ah, 'w', 'x', 'c', 'v', 'b'
+    db 'n', ',', ';', ':', '!', 0
+
+    ; Sector padding to have a bin generated of 512 bytes (1 sector)
     times 512-($-$$) db 0
