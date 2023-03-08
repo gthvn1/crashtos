9b95bc471a00549463dbc545998c7ca9f6d457c8
Guillaume <gthvn1@gmail.com>
2023-03-08 10:53:13 +0100

    prepare the transition to 32bits
    To prepare the transition we really keep all 16 bits stuff in the
    bootloader. We removed all inclusion to control what will be in...

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -4,12 +4,11 @@
 ;; It will load the stage2 by reading the second sector on the first
 ;; disk.
 
-%include "include/constants.asm"
-
-    org BOOTLO_OFFSET ; The code is loaded at 0x7C00 by the bootloader
-                      ; We need to set it otherwise when later in the code
-                      ; we will refer to memory location the address will be
-                      ; wrong. For example mov al, [outputChar] will not work.
+[BITS 16]    ; This is the real world
+[ORG 0x7C00] ; The code is loaded at 0x7C00 by the bootloader
+             ; We need to set it otherwise when later in the code
+             ; we will refer to memory location the address will be
+             ; wrong. For example mov al, [outputChar] will not work.
 
 ;; MEMORY LAYOUT
 ;; https://wiki.osdev.org/Memory_Map_(x86)
@@ -34,10 +33,11 @@
 ;; we will access file table data from stage2 we need to make far jump.
 
     ; Setup the stack under us
-    mov bp, BOOTLO_OFFSET
-    mov ax, BOOTLO_SEG
+    mov bp, 0x7C00
+    mov ax, 0x0000
     mov ss, ax
-    mov sp, bp
+    mov sp, bp  ; So stack is growing down from 0x0000:0x7C00 -> 0x0000:0x0500
+                ; We don't check if it smashes BDA but 30KB is really huge...
 
     ; Setup video mode
     mov ah, 0x0 ; Set BIOS service to "set video mode"
@@ -47,40 +47,82 @@
     ; First we will File Table from sector 2 at 0x1000:0x0000
     ; DON'T use load_file to load the file table because load_file relies on
     ; the fileTable IN MEMORY to load things...
-    mov bx, FTABLE_SEG
+    mov bx, 0x1000
     mov es, bx            ; es <- 0x1000
     xor bx, bx            ; bx <- 0x0
                           ; Set [es:bx] to 0x0001:0x0000,
 
     mov cx, 0x00_02       ; Cylinder: 0, Sector: 2
-    mov al, 0x1           ; Read one sector (512 bytes)
+    mov al, 0x1           ; Load 1 sector (512 bytes)
     call load_disk_sector ; Read the file table from disk
 
     ; Now we can load the stage2 from sector 3 at 0x1000:0x0200
-    push stage2Name
-    push STAGE2_SEG
-    push STAGE2_OFFSET
-    call load_file
-    cmp ax, 0
-    jne fatal_error
+    mov bx, 0x1000
+    mov es, bx
+    mov bx, 0x0200   ; [ES:BX] <- 0x1000:0x0200
+    mov cx, 0x00_03  ; Cylinder 0, Sector 3
+    mov al, 0x4      ; Load 4 sectors (2KB)
+    call load_disk_sector ; Read the file table from disk
 
     ; before jumping to the stage2 we need to setup segments
-    mov ax, STAGE2_SEG
+    mov ax, 0x1000
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    jmp STAGE2_SEG:STAGE2_OFFSET ; far jump to stage2
+    jmp 0x1000:0x0200 ; far jump to stage2
 
-    ; Should not be reached because we never returned from stage2 space...
+;; ============================================================================
+;; load_disk_sector
+;; Inputs:
+;;   - AL: Number of sectors to be read
+;;   - CH: Cylinder
+;;   - CL: Sector
+;;   - [ES:BX] the memory where we want to load the sector
+;; Clobbers:
+;;   - SI, AH, DX
+
+load_disk_sector:
+    ;  - es:bx are set set before calling it
+    mov si, 0x3 ; disk reads should be retried at least three times
+                ; we use SI because all others registers are needed
+
+    ; Reset the disk before reading it
+    mov ah, 0x0
+    int 0x13
+
+    mov ah, 0x2  ; BIOS service: read sectors from drive
+                 ; AL is set when calling load_disk_sector
+                 ; CH & CL are also already set
+    mov dh, 0x0  ; Head 0
+    mov dl, 0x80 ; Read first hard drive
+
+    int 0x13     ; 0x13 BIOS service
+
+    ; Check the result
+    ; If CF == 1 then there is an error
+    jc .failed_to_load_stage2
+
+    ret
+
+.failed_to_load_stage2:
+    dec si
+    jnz load_disk_sector ; if it is not zero we can retry
+
+    ; We failed more than 3 times, it is over !!!
+    cli
+    hlt
+
+    times 510-($-$$) db 0    ; padding with 0s
+    dw 0xaa55        ; BIOS magic number
+
+; =====================[ KINGDOM OF PROTECTED WORLD ]===========================
+
+[BITS 32] ; This is the protected kingdom... Currently we cannot cross that line
+          ; because in this world we need to get GDT sword.... Crossing this
+          ; border now will be fatal !!!!
+
 fatal_error:
     cli
     hlt
 
-%include "include/load_file.asm"
-%include "include/load_disk_sector.asm"
-
-stage2Name db "stage2", 0
-
-    times 510-($-$$) db 0    ; padding with 0s
-    dw 0xaa55        ; BIOS magic number
